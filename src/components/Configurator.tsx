8441a4a254e404e1e271343bd973c2320e73f4b2
Pashafd <pasha.yakubovsky@lynksen.com>
2023-09-11 19:56:28 +0300

    +rewrite configurator on flex

--- src/components/Configurator.tsx
+++ src/components/Configurator.tsx
@@ -7,25 +7,29 @@ const Configurator = () => {
             <h2>Konfigurator</h2>
 
             <div className={styles.grid}>
-                <div className={styles.gridText}>
-                    <p>
-                        Der Konfigurator ist ein Tool von antwerpes, der es den Kunden der Agentur
-                        ermöglicht, schnell und bequem eine Kampagnenanfrage zu senden und dabei
-                        deren Details und Budget anzugeben.{" "}
-                    </p>
-                    <p>
-                        Ich sollte das Design der Website unter Berücksichtigung der
-                        Benutzerfreundlichkeit und der technischen Anforderungen aktualisieren.
-                    </p>
+                <div>
+                    <div className={styles.gridText}>
+                        <p>
+                            Der Konfigurator ist ein Tool von antwerpes, der es den Kunden der
+                            Agentur ermöglicht, schnell und bequem eine Kampagnenanfrage zu senden
+                            und dabei deren Details und Budget anzugeben.{" "}
+                        </p>
+                        <p>
+                            Ich sollte das Design der Website unter Berücksichtigung der
+                            Benutzerfreundlichkeit und der technischen Anforderungen aktualisieren.
+                        </p>
+                    </div>
+
+                    <div className={styles.gridImage}>
+                        <Image
+                            objectFit="contain"
+                            src="/images/configurator3.jpg"
+                            fill
+                            alt="configurator image"
+                        />
+                    </div>
                 </div>
-                <div className={styles.gridImage}>
-                    <Image
-                        objectFit="contain"
-                        src="/images/configurator3.jpg"
-                        fill
-                        alt="configurator image"
-                    />
-                </div>
+
                 <div className={styles.gridBottom}>
                     <Image
                         objectFit="contain"
